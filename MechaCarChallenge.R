b65edfab316855243a77156135641b8fb96f4172
Humberto Rodriguez <78564912+harg74@users.noreply.github.com>
2022-01-03 07:10:58 -0600

    Module Challenge 15

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,5 +1,11 @@
 library(dplyr)
-used_cars <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F)
+
+#DELIVERABLE 1
+
+# import file
+used_cars <- read.csv('MechaCar_mpg.csv', stringsAsFactors = F)
+
+#head of dataframe
 head(used_cars)
 
 #generate multiple linear regression model (coefficients)
@@ -7,3 +13,18 @@ lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AW
 
 #generate summary statistics
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=used_cars))
+
+
+#DELIVERABLE 2
+
+#import file
+suspension_coil <- read.csv('Suspension_Coil.csv', stringsAsFactors = F)
+
+#head of dataframe
+head(suspension_coil)
+
+#summarize dataframe
+total_summary <- summarize(suspension_coil, Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI) , SD= sd(PSI), .groups = 'keep')
+
+lot_summary <- suspension_coil %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI), Median_PSI=median(PSI), Variance=var(PSI) , SD= sd(PSI), .groups = 'keep')
+
